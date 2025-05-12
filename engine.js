cfafc3a08fa6028c5708879924f1e3b3b0514ea7
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-05-12 19:03:08 +0300

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,38 @@
+class VisualNovelEngine {
+    constructor() {
+        this.currentScene = null;
+        this.scenesData = {};
+        this.mainDiv = document.getElementById('main');
+        this.setupEventDelegation();
+    }
+
+    init(gameData) {
+        this.scenesData = gameData.scenes;
+        this.startVisualNovel();
+    }
+
+    setupEventDelegation() {
+        this.mainDiv.addEventListener('click', (e) => {
+            const elementWithNextScene = e.target.closest('[next_scene]');
+            if (elementWithNextScene) {
+                const targetScene = elementWithNextScene.getAttribute('next_scene');
+                this.renderScene(targetScene);
+            }
+        });
+    }
+
+    startVisualNovel() {
+        this.renderScene('start_screen');
+    }
+
+    renderScene(sceneId) {
+        const scene = this.scenesData[sceneId];
+        this.currentScene = sceneId;
+        this.mainDiv.innerHTML = scene.html || '';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.vnEngine = new VisualNovelEngine();
+    vnEngine.init(gameData);
+});
